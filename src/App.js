3a143fd43fb7045b91fcaebffce4c89fa61ddd9d
Eman Jamal <emanjse@gmail.com>
2022-09-01 23:55:58 -0700

    add react-router-dom@6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Routes, Route} from "react-router-dom";
 import './App.css';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
 
 export default function App() {
   return (
-    <div> <h1 className="text-3xl font-bold">
-    Hello world!
-  </h1></div>
+    <div>
+        <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<Menu />} />
+      </Routes>
+    </div>
 
   );
 }
