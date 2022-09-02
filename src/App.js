a62b9bd62297bfb481ee40c8825c198cec0961ed
Eman Jamal <emanjse@gmail.com>
2022-09-01 22:49:18 -0700

    add @reduxjs/toolkit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <div> <h1 className="text-3xl font-bold underline">
+    <div> <h1 className="text-3xl font-bold">
     Hello world!
   </h1></div>
 
