91a45bfbcf829c2f9adeaba9ad09c3a6a5b6fb3d
Eman Jamal <emanjse@gmail.com>
2022-09-17 09:34:55 -0700

    create Contact Page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import '../Style/BookTable.css';
 
 
 function Contact() {
-    return(
-        <section>
-        hi
-        </section>
-    )
+
+  return (
+    <section class="form" data-aos="fade-up">
+    <div class="container">
+
+
+
+      <div class="form__wrapper">
+
+        <form name="booking" method="POST" netlify>
+          <div class="form__group">
+            <label for="firstName">Your Name</label>
+            <input type="text"  name="Your Name" required/>
+          </div>
+
+          <div class="form__group">
+            <label for="secondtName">Second Name</label>
+            <input type="text"  name="Second Name" required/>
+          </div>
+
+
+
+
+          <div class="form__group">
+            <label for="email">Email</label>
+            <input type="email"  name="Email" required/>
+          </div>
+
+
+          <div class="form__group">
+            <label for="Type">What about</label>
+            <select name="Type" required>
+               <option value="english village">General</option>
+                <option value="ankawa">FeedBack</option>
+                <option value="pirmam">Inquiry</option>
+                <option value="esport">Other..</option>
+            </select>
+          </div>
+
+
+          <div class="form__group form__group__full">
+            <label for="note">Note</label>
+            <textarea name="Note"  rows="4"></textarea>
+          </div>
+        </form>
+        <button class="btn4 mt-4">Submit</button>
+
+      </div>
+    </div>
+  </section>
+  )
 }
+
 export default Contact;
